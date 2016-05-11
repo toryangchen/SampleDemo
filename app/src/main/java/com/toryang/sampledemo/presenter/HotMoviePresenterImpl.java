c6fbe759429627ba19b7d071b75778c7172cea2e
toryang <ToryangChen@outlook.com>
2016-05-11 17:06:45 +0800

    updata UI with hot movie fragment

--- app/src/main/java/com/toryang/sampledemo/presenter/HotMoviePresenterImpl.java
+++ app/src/main/java/com/toryang/sampledemo/presenter/HotMoviePresenterImpl.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
